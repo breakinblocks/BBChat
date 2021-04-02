71a0ce8bedfc99e86889c82ca39d556864f956e9
BlueAgent <BlueAgent@users.noreply.github.com>
2021-04-02 12:18:54 +0800

    Add commands from text files

--- bbchat-common/src/main/java/com/breakinblocks/bbchat/common/ChatRelay.java
+++ bbchat-common/src/main/java/com/breakinblocks/bbchat/common/ChatRelay.java
@@ -22,6 +22,11 @@ import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nullable;
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
@@ -43,6 +48,7 @@ public class ChatRelay implements IRelay {
     private static final String FORMAT_LOGOUT = BOLD + "%s" + RESET + " left the server";
     private static final String FORMAT_ACHIEVEMENT = BOLD + "%s" + RESET + " got " + BOLD + "%s" + RESET + " " + ITALIC + "%s" + RESET;
     private static final Pattern REGEX_EMOTE = Pattern.compile(":([A-Za-z0-9_]{2,32}):");
+    private static final int MAX_COMMAND_FILE_SIZE = 128 * 1024; // 128 KB should be plenty
     private final JDA jda;
     private final long guildId;
     private final long channelId;
@@ -142,7 +148,34 @@ public class ChatRelay implements IRelay {
         final String prefix = commandPrefixes.stream().filter(rawMessage::startsWith).findAny().orElse(null);
         // Starts with the prefix or mentions the relay
         if (prefix == null) return;
-        final String fullCommand = rawMessage.substring(prefix.length()).trim();
+        final String commandPortion = rawMessage.substring(prefix.length()).trim();
+        // If it's empty without the prefix, try get the command from the first attached txt file instead
+        if (commandPortion.isEmpty()) {
+            message.getAttachments().stream()
+                    .filter(attachment -> attachment.getSize() <= MAX_COMMAND_FILE_SIZE)
+                    .filter(attachment -> "txt".equals(attachment.getFileExtension()))
+                    .findFirst()
+                    .map(Message.Attachment::retrieveInputStream)
+                    .ifPresent(future -> future
+                            .thenApply(stream -> {
+                                try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+                                    CharBuffer buffer = CharBuffer.allocate(MAX_COMMAND_FILE_SIZE);
+                                    int len = reader.read(buffer);
+                                    buffer.rewind();
+                                    return buffer.subSequence(0, len).toString();
+                                } catch (IOException e) {
+                                    LOGGER.error("Failed to read attachment", e);
+                                }
+                                return "";
+                            })
+                            .thenAccept(command -> handlePotentialCommand(member, command.trim()))
+                    );
+        } else {
+            handlePotentialCommand(member, commandPortion);
+        }
+    }
+
+    private void handlePotentialCommand(Member member, String fullCommand) {
         final boolean isStaff = member.getRoles().stream().anyMatch(role -> role.getIdLong() == staffRoleId);
         final String commandRoot = fullCommand.split(" ", 2)[0];
         // Check that it is a staff member or the command is allowed for anyone
