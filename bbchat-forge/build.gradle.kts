33a4350aad8c7cef907c047a406c8f516bab3466
BlueAgent <BlueAgent@users.noreply.github.com>
2021-12-30 17:28:01 +0800

    Copy bbchat-forge to bbchat-forge-1.17.1 (restore)

---
+++ bbchat-forge/build.gradle.kts
@@ -0,0 +1,113 @@
+@file:Suppress("PropertyName")
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.minecraftforge.gradle.userdev.UserDevExtension
+import net.minecraftforge.gradle.userdev.tasks.RenameJarInPlace
+
+val mod_version: String by project
+val mc_version: String by project
+val mc_version_range_supported: String by project
+val forge_version: String by project
+val forge_version_range_supported: String by project
+val mappings_channel: String by project
+val mappings_version: String by project
+
+plugins {
+    id("com.github.ben-manes.versions")
+    id("com.github.johnrengelman.shadow")
+    id("net.minecraftforge.gradle")
+    id("org.parchmentmc.librarian.forgegradle")
+}
+
+base.archivesName.set("bbchat-${mc_version}")
+
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+
+configure<UserDevExtension> {
+    mappings(mappings_channel, mappings_version)
+    runs {
+        create("client") {
+            workingDirectory(file("run"))
+            property("forge.logging.markers", "SCAN,REGISTRIES,REGISTRYDUMP")
+            property("forge.logging.console.level", "debug")
+            mods {
+                create("bbchat") {
+                    sources = listOf(sourceSets["main"])
+                }
+            }
+        }
+        create("server") {
+            workingDirectory(file("run"))
+            property("forge.logging.markers", "SCAN,REGISTRIES,REGISTRYDUMP")
+            property("forge.logging.console.level", "debug")
+
+            mods {
+                create("bbchat") {
+                    sources = listOf(sourceSets["main"])
+                }
+            }
+        }
+        create("data") {
+            workingDirectory(file("run"))
+            property("forge.logging.markers", "SCAN,REGISTRIES,REGISTRYDUMP")
+            property("forge.logging.console.level", "debug")
+            setArgs(listOf("--mod", "bbchat", "--all", "--output", file("src/generated/resources/")))
+            mods {
+                create("bbchat") {
+                    sources = listOf(sourceSets["main"])
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    add("minecraft", "net.minecraftforge:forge:${mc_version}-${forge_version}")
+    implementation(project(path = ":bbchat-common", configuration = "shadow"))
+}
+
+tasks.named<ProcessResources>("processResources") {
+    inputs.property("mod_version", mod_version)
+    inputs.property("mc_version_range_supported", mc_version_range_supported)
+    inputs.property("forge_version_range_supported", forge_version_range_supported)
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    from(sourceSets["main"].resources.srcDirs) {
+        include("META-INF/mods.toml")
+        expand(
+            "mod_version" to mod_version,
+            "mc_version_range_supported" to mc_version_range_supported,
+            "forge_version_range_supported" to forge_version_range_supported
+        )
+    }
+    from(sourceSets["main"].resources.srcDirs) {
+        exclude("META-INF/mods.toml")
+    }
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes(
+            "Specification-Title" to "BBChat",
+            "Specification-Vendor" to "Breakin' Blocks",
+            "Specification-Version" to "1",
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version,
+            "Implementation-Vendor" to "Breakin' Blocks"
+        )
+    }
+}
+
+val shadowJar = tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set("forge")
+    dependencies {
+        include(project(":bbchat-common"))
+    }
+}
+
+extensions.configure<NamedDomainObjectContainer<RenameJarInPlace>> {
+    create("shadowJar")
+}
+
+tasks.named<DefaultTask>("build") {
+    dependsOn(shadowJar)
+}
