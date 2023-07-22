9853ffd00a1258b0cf25ed9d9d4f78e19d24e79e
BlueAgent <BlueAgent@users.noreply.github.com>
2023-07-22 17:42:55 +0800

    Use our version of fabric loader and similar changes to quilt to get server running in-dev

--- projects/minecraft/v1.18.2/fabric/build.gradle.kts
+++ projects/minecraft/v1.18.2/fabric/build.gradle.kts
@@ -21,6 +21,17 @@ val parentPath = Path.path(project.path).parent!!.path!!
 val vanillaPath = Path.path(parentPath).child("vanilla").path!!
 evaluationDependsOn(vanillaPath)
 
+val includeModApi: Configuration by configurations.creating
+
+configurations {
+    configurations.include {
+        extendsFrom(includeModApi)
+    }
+    configurations.modApi {
+        extendsFrom(includeModApi)
+    }
+}
+
 dependencies {
     minecraft("com.mojang:minecraft:${minecraft_version}")
     mappings(loom.layered {
@@ -28,12 +39,18 @@ dependencies {
         this.parchment("org.parchmentmc.data:parchment-${parchment_minecraft_version}:${parchment_version}@zip")
     })
     modImplementation("net.fabricmc:fabric-loader:${fabric_loader_version}")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabric_api_version}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabric_api_version}") {
+        exclude("net.fabricmc", "fabric-loader")
+    }
     implementation("com.google.code.findbugs:jsr305:3.0.2")
     implementation(project(path = corePath, configuration = "shadow"))
     compileOnly(project(path = vanillaPath))
-    modApi("net.minecraftforge:forgeconfigapiport-fabric:${forge_config_api_port_version}")
-    include("net.minecraftforge:forgeconfigapiport-fabric:${forge_config_api_port_version}")
+    includeModApi("net.minecraftforge:forgeconfigapiport-fabric:${forge_config_api_port_version}") {
+        exclude("net.fabricmc")
+        exclude("net.fabricmc.fabric-api")
+    }
+    compileOnly("com.electronwill.night-config:core:3.6.3")
+    compileOnly("com.electronwill.night-config:toml:3.6.3")
 }
 
 tasks.withType<JavaCompile> {
