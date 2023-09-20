d5fc84c4a058a9654ce55e9a0796318c4097fec8
BlueAgent <BlueAgent@users.noreply.github.com>
2023-09-21 05:07:04 +0800

    Fix level access for latest

--- projects/minecraft/latest/vanilla/src/main/java/com/breakinblocks/bbchat/vanilla/BBChat.java
+++ projects/minecraft/latest/vanilla/src/main/java/com/breakinblocks/bbchat/vanilla/BBChat.java
@@ -71,7 +71,7 @@ public class BBChat {
         DisplayInfo displayInfo = advancement.getDisplay();
         if (displayInfo == null) return;
         if (!displayInfo.shouldAnnounceChat()) return;
-        if (!player.level.getGameRules().getBoolean(GameRules.RULE_ANNOUNCE_ADVANCEMENTS)) return;
+        if (!player.level().getGameRules().getBoolean(GameRules.RULE_ANNOUNCE_ADVANCEMENTS)) return;
         String name = player.getName().getString();
         String title = displayInfo.getTitle().getString();
         String description = displayInfo.getDescription().getString();
@@ -83,7 +83,7 @@ public class BBChat {
      */
     protected void relayDeath(LivingEntity livingEntity, DamageSource damageSource) {
         if (isRealPlayer(livingEntity) || (livingEntity.hasCustomName() && isRealPlayer(damageSource.getEntity()))) {
-            if (!livingEntity.level.getGameRules().getBoolean(GameRules.RULE_SHOWDEATHMESSAGES)) return;
+            if (!livingEntity.level().getGameRules().getBoolean(GameRules.RULE_SHOWDEATHMESSAGES)) return;
             String deathMessage = livingEntity.getCombatTracker().getDeathMessage().getString();
             String target = livingEntity.getName().getString();
             Entity sourceEntity = damageSource.getEntity();
